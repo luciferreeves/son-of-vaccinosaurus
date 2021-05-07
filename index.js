fa364c62fcb107c735bde9fcdc0d2e6d7272dcff
rohitsingh11 <rohit@twodotseven.com>
2021-05-07 21:08:44 +0530

    added calenderbypin and calenderbydistrict functions

--- index.js
+++ index.js
@@ -1,37 +1,63 @@
-const express = require('express');
-const cors = require('cors')({
-    origin: true
+const express = require("express");
+const cors = require("cors")({
+  origin: true,
 });
-const axios = require('axios');
+const axios = require("axios");
 const app = express();
 const port = 4000;
 
 app.use(cors);
-app.options('*', cors);
+app.options("*", cors);
 
-app.get('/', (req, res) => {
-    res.send('Son of Vaccinosaurus says Grrrrr!');
-})
+app.get("/", (req, res) => {
+  res.send("Son of Vaccinosaurus says Grrrrr!");
+});
 
-// app.get('/', (req, res) => {
-    // var config = {
-    //     method: 'get',
-    //     url: 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=700016&date=07-05-2021',
-    //     headers: {
-    //         'accept': 'application/json',
-    //         'Accept-Language': 'hi_IN'
-    //     }
-    // };
+app.get("/calenderByPin", (req, res) => {
+  const pincode = req.query.pincode;
+  const date = req.query.date;
+  const config = {
+    method: "get",
+    url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${date}`,
+    headers: {
+      accept: "application/json",
+      "Accept-Language": "hi_IN",
+      "user-agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36",
+    },
+  };
 
-    // axios(config)
-    //     .then(function (response) {
-    //         console.log(JSON.stringify(response.data));
-    //     })
-    //     .catch(function (error) {
-    //         console.log(error);
-    //     });
-// })
+  axios(config)
+    .then(function (response) {
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      res.send(error);
+    });
+});
+app.get("/calenderByDistrict", (req, res) => {
+  const districtID = req.query.district_id;
+  const date = req.query.date;
+  const config = {
+    method: "get",
+    url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${districtID}&date=${date}`,
+    headers: {
+      accept: "application/json",
+      "Accept-Language": "hi_IN",
+      "user-agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36",
+    },
+  };
+
+  axios(config)
+    .then(function (response) {
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      res.send(error);
+    });
+});
 
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
-})
+  console.log(`App listening at http://localhost:${port}`);
+});
